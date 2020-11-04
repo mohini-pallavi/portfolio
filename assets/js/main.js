1c3c16f9096c7341f8087d831ffdee0130997404
PALLAVI MOHINI <1804395@kiit.ac.in>
2020-11-05 00:07:55 +0530

    final

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,61 +1,64 @@
-/*===== MENU SHOW =====*/ 
-const showMenu = (toggleId, navId) =>{
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+const submit = document.querySelector(".contact__button");
 
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
-            nav.classList.toggle('show')
-        })
-    }
-}
-showMenu('nav-toggle','nav-menu')
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  localStorage.setItem("formValues", JSON.stringify(store()));
 
-/*===== ACTIVE AND REMOVE MENU =====*/
-const navLink = document.querySelectorAll('.nav__link');   
-
-function linkAction(){
-  /*Active link*/
-  navLink.forEach(n => n.classList.remove('active'));
-  this.classList.add('active');
-  
-  /*Remove menu mobile*/
-  const navMenu = document.getElementById('nav-menu')
-  navMenu.classList.remove('show')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction));
-
-/*===== SCROLL REVEAL ANIMATION =====*/
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '80px',
-    duration: 2000,
-    reset: true
+  alert("form submitted");
 });
 
-/*SCROLL HOME*/
-sr.reveal('.home__title',{}); 
-sr.reveal('.button',{delay: 200}); 
-sr.reveal('.home__img',{delay: 400}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
+function store() {
+  let inputName = document.querySelector(".txt").value;
+  let inputEmail = document.querySelector(".mail").value;
+  let inputNumber = +document.querySelector(".nmbr").value;
+  let inputReason = document.querySelector(".txtar").value;
 
-/*SCROLL ABOUT*/
-sr.reveal('.about__img',{}); 
-sr.reveal('.about__subtitle',{delay: 400}); 
-sr.reveal('.about__text',{delay: 400}); 
+  const formValues = {
+    inputName: inputName,
+    inputEmail: inputEmail,
+    inputNumber: inputNumber,
+    inputReason: inputReason,
+  };
 
-/*SCROLL SKILLS*/
-sr.reveal('.skills__subtitle',{}); 
-sr.reveal('.skills__text',{}); 
-sr.reveal('.skills__data',{interval: 200}); 
-sr.reveal('.skills__img',{delay: 600});
+  return formValues;
+}
 
-/*SCROLL WORK*/
-sr.reveal('.work__img',{interval: 200}); 
+const take = document.getElementById("txttype");
 
-/*SCROLL CONTACT*/
-sr.reveal('.contact__input',{interval: 200}); 
+let typewriter = new Typewriter(take, {
+  loop: true,
+  delay: 55,
+});
+let arr = ["Developer", "Coder", "Content-Writer", "Speaker"];
+for (let i = 0; i < arr.length; i++) {
+  let s = arr[i];
+  s.toString();
 
+  typewriter
+    .pauseFor(500)
+    .typeString(s)
+    .pauseFor(500)
+    .deleteChars(15)
+    .pauseFor(800)
+    .start();
+}
 
+//Get the button:
+mybutton = document.getElementById("myBtn");
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
 
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
